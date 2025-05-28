60272e18072a5d3534aa20a31a44d2ce00b3af3f
Yaroslav Olieniev <olieniev.od.ua@gmail.com>
2025-05-28 12:54:33 +0200

    impl accommodation model and liquibase scripts

---
+++ src/main/java/com/olieniev/bookingapp/model/Accommodation.java
@@ -0,0 +1,65 @@
+package com.olieniev.bookingapp.model;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Table(name = "accommodations")
+@SQLDelete(sql = "UPDATE accommodations SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
+@Getter
+@Setter
+@ToString
+public class Accommodation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String size;
+
+    @Column(nullable = false, name = "amenity")
+    @ElementCollection
+    @CollectionTable(name = "accommodations_amenities",
+            joinColumns = @JoinColumn(name = "accommodation_id"))
+    private Set<String> amenities = new HashSet<>();
+
+    @Column(nullable = false)
+    private BigDecimal dailyRate;
+
+    @Column(nullable = false)
+    private Integer availability;
+
+    public enum Type {
+        HOTEL,
+        HOUSE,
+        APARTMENT,
+        CONDO,
+        VACATION_HOME
+    }
+}
